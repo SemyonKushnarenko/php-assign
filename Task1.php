19a245ac0a57df2a7647096c78059dcfce29d0ac
Trisemya <trisemya@gmail.com>
2022-09-02 11:00:34 +0300

    feat: add exception

--- Task1.php
+++ Task1.php
@@ -2,11 +2,14 @@
 
 namespace src;
 
+use InvalidArgumentException;
+
 class Task1 {
     public function main(int $inputNumber): string
     {
-      return ($inputNumber > 30) ? 'More than 30' : 
-          (($inputNumber > 20) ? 'More than 20' :
-          (($inputNumber > 10) ? 'More than 10' : 'Equal or less than 10'));
+        if (!is_int($inputNumber)) {
+            throw new InvalidArgumentException('You must input integer value');
+        }
+        return ($inputNumber > 30) ? 'More than 30' : (($inputNumber > 20) ? 'More than 20' : (($inputNumber > 10) ? 'More than 10' : 'Equal or less than 10'));
     }
 }
