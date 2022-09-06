c1a40b7f8a7107034fc643a1a0d3543bf46f748e
Trisemya <trisemya@gmail.com>
2022-09-06 11:51:20 +0300

    feat: add task 8 implementation

---
+++ Task8.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace src;
+
+use InvalidArgumentException;
+
+class Task8
+{
+    public $str = '';
+    public function main(string $inputStr)
+    {
+        if (!is_string($inputStr)) {
+            throw new InvalidArgumentException('You must input string value');
+        }
+
+        $obj = json_decode($inputStr);
+
+
+        return $this->objToStr($obj);
+    }
+
+    public function objToStr($obj)
+    {
+        foreach ($obj as $key => $value) {
+            if (is_string($value)) {
+                $this->str = $this->str . "$key: $value<br>";
+            } else {
+                $this->str = $this->objToStr($value);
+            }
+        }
+
+        return $this->str;
+    }
+}
