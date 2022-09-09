b3b4a171ebb2cf6a1ea70b8e666fe5d4acf354f5
Simon <trisemya@gmail.com>
2022-09-09 19:11:58 +0300

    fix: fix task-1 checks

--- Task1.php
+++ Task1.php
@@ -2,16 +2,10 @@
 
 namespace src;
 
-use InvalidArgumentException;
-
 class Task1
 {
     public function main(int $inputNumber): string
     {
-        if (!is_int($inputNumber)) {
-            throw new InvalidArgumentException('You must input integer value');
-        }
-
         return ($inputNumber > 30) ? 'More than 30' : (($inputNumber > 20) ? 'More than 20' : (($inputNumber > 10) ? 'More than 10' : 'Equal or less than 10'));
     }
 }
