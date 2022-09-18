bb74b557603949cb32d26d92c5ad965e6827718c
Simon <trisemya@gmail.com>
2022-09-19 00:41:26 +0300

    feat: change checks

--- Task6.php
+++ Task6.php
@@ -3,13 +3,20 @@
 namespace src;
 
 use DateTime;
+use InvalidArgumentException;
 
 class Task6
 {
     public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
+        if ($lastYear < 1 && $year < 1) {
+            throw new InvalidArgumentException('You must input year and year of the end');
+        }
         $startDate = new DateTime("$year-$month");
         $endDate = new DateTime("$lastYear-$lastMonth");
+        if ($startDate > $endDate) {
+            throw new InvalidArgumentException('You must input year and year of the end');
+        }
         $i = 0;
         while ($startDate < $endDate) {
             $startDate = $startDate->modify('+1 month');
