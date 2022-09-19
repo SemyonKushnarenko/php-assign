7c135eecac6c6a65deaecdcbd0d14ddee8446f2d
Simon <trisemya@gmail.com>
2022-09-19 12:31:44 +0300

    feat: fix check

--- Task7.php
+++ Task7.php
@@ -8,7 +8,7 @@ class Task7
 {
     public function main(array $inputArr, int $position): array
     {
-        if (!count($inputArr) || $position > count($inputArr)) {
+        if (!count($inputArr) || $position > count($inputArr) - 1 || $position < 0) {
             throw new InvalidArgumentException('You must input array and position of the element of this array');
         }
 
